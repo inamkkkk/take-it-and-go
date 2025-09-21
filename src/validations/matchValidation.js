ebf031bbdbd9e85bf97fb01ab3b9a325f1124186
inamkkkk <inamk9025@gmail.com>
2025-09-22 03:35:51 +0500

    fix: refine src/validations/matchValidation.js

--- src/validations/matchValidation.js
+++ src/validations/matchValidation.js
@@ -3,11 +3,16 @@ const { objectId } = require('./customValidation');
 
 const findMatches = {
   body: Joi.object().keys({
-    deliveryId: Joi.string().custom(objectId).required()
-    // TODO: Add more criteria for matching, e.g.,
-    // origin: Joi.object().keys({ latitude: Joi.number(), longitude: Joi.number() }),
-    // destination: Joi.object().keys({ latitude: Joi.number(), longitude: Joi.number() }),
-    // itemType: Joi.string().optional()
+    deliveryId: Joi.string().custom(objectId).required(),
+    origin: Joi.object().keys({
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required()
+    }).optional(),
+    destination: Joi.object().keys({
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required()
+    }).optional(),
+    itemType: Joi.string().optional()
   })
 };
 
