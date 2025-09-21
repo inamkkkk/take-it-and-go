4a55b8d384594152e48eb4cead924c2988b92b58
inamkkkk <inamk9025@gmail.com>
2025-09-22 03:33:36 +0500

    fix: refine src/utils/appError.js

--- src/utils/appError.js
+++ src/utils/appError.js
@@ -8,6 +8,7 @@ class AppError extends Error {
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
     this.isOperational = true;
 
+    // Capture the stack trace, excluding the constructor call from it.
     Error.captureStackTrace(this, this.constructor);
   }
 }
