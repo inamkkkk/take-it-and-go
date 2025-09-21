182d77e8144933ace024a79a54d9f088bdb17e56
inamkkkk <inamk9025@gmail.com>
2025-09-22 03:30:57 +0500

    fix: refine src/routes/authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -9,4 +9,10 @@ router.post('/signup', validate(authValidation.signup), authController.signup);
 router.post('/login', validate(authValidation.login), authController.login);
 router.post('/reset-password', validate(authValidation.resetPassword), authController.resetPassword);
 
+// TODO: Add a route for password reset confirmation
+// router.post('/reset-password-confirm', validate(authValidation.resetPasswordConfirm), authController.resetPasswordConfirm);
+
+// TODO: Add a route for refreshing JWT tokens
+// router.post('/refresh-token', authController.refreshToken);
+
 module.exports = router;
