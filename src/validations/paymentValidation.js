171ebb9588e4dc7f2a171f2ff415d7d2c87f512d
inamkkkk <inamk9025@gmail.com>
2025-09-22 03:36:11 +0500

    fix: refine src/validations/paymentValidation.js

--- src/validations/paymentValidation.js
+++ src/validations/paymentValidation.js
@@ -3,18 +3,19 @@ const { objectId } = require('./customValidation');
 
 const createEscrow = {
   body: Joi.object().keys({
-    deliveryId: Joi.string().custom(objectId).required(),
-    amount: Joi.number().min(0).required()
-  })
-};
+    deliveryId: Joi.string().custom(objectId).required().messages({
+      'any.required': 'Delivery ID is required.',
+      'string.pattern.base': 'Delivery ID must be a valid ObjectId.'}),
+    amount: Joi.number().min(0).required().messages({
+      'any.required': 'Amount is required.',
+      'number.min': 'Amount cannot be negative.'})})};
 
 const releaseFunds = {
   body: Joi.object().keys({
-    paymentId: Joi.string().custom(objectId).required()
-  })
-};
+    paymentId: Joi.string().custom(objectId).required().messages({
+      'any.required': 'Payment ID is required.',
+      'string.pattern.base': 'Payment ID must be a valid ObjectId.'})})};
 
 module.exports = {
   createEscrow,
-  releaseFunds
-};
+  releaseFunds};
