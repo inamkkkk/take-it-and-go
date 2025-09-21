9dd1a622e08f88cd65cbdc40e8f77e113c67a0fb
inamkkkk <inamk9025@gmail.com>
2025-09-22 03:29:19 +0500

    fix: refine src/controllers/matchController.js

--- src/controllers/matchController.js
+++ src/controllers/matchController.js
@@ -5,7 +5,8 @@ const httpStatus = require('http-status-codes');
 const findMatches = catchAsync(async (req, res) => {
   const { deliveryId } = req.body;
   // TODO: req.user.id can be used to ensure only the shipper of delivery can find matches
-  const matches = await matchService.findMatches(deliveryId);
+  // For now, assuming the service handles delivery ownership validation or it's not a requirement yet.
+  const matches = await matchService.findMatches(deliveryId, req.user.id); // Passing req.user.id to the service for potential validation
   res.status(httpStatus.OK).send(matches);
 });
 
